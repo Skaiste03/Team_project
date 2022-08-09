7cbf9adcfe190bffe20de65c2d74e68f672a18f6
Rokas <rokasgiedraitis@gmail.com>
2022-08-09 23:41:42 +0300

    Created react router logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ROUTES from './shared/constants/routes';
 import './App.css';
+import Navigation from './components/organisms/Navigation';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Suspense>
+        <Routes>
+          {ROUTES.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 };
 
 export default App;
