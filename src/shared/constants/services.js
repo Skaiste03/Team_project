b2030d66dec1ccee85ff6eb4b31ea8c6ede2054b
Rokas <rokasgiedraitis@gmail.com>
2022-08-10 19:34:54 +0300

    Created services.js constants file

---
+++ src/shared/constants/services.js
@@ -0,0 +1,12 @@
+import { uid } from 'uid';
+
+const SERVICES = [
+  { id: uid(), text: '', logo: '' },
+  { id: uid(), text: '', logo: '' },
+  { id: uid(), text: '', logo: '' },
+  { id: uid(), text: '', logo: '' },
+  { id: uid(), text: '', logo: '' },
+  { id: uid(), text: '', logo: '' },
+  { id: uid(), text: '', logo: '' },
+  { id: uid(), text: '', logo: '' },
+];
