ee186e4f1ad7bee7badbbad73ceb8b789dd16e81
Skaiste <smacaityte@gmail.com>
2022-08-10 22:01:16 +0300

    Changed quotation marks to a single ones in file

--- src/components/organisms/Footer/Footer.style.jsx
+++ src/components/organisms/Footer/Footer.style.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import BREAKPOINTS from "../../../shared/constants/breakpoints";
+import styled from 'styled-components';
+import BREAKPOINTS from '../../../shared/constants/breakpoints';
 
 export const StyledFooter = styled.footer`
   width: calc((100vw / 100%) - 100vw);
