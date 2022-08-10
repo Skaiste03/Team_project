3665f636f880409c1ff62d439924790f8afecfe0
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 01:04:33 +0300

    Updated App.js, removed testing code and added CallToAction component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import ROUTES from './shared/constants/routes';
 import './App.css';
 import Header from './components/organisms/Header/Header';
 import Footer from './components/organisms/Footer/Footer';
-import SERVICES from './shared/constants/services';
-import Card from './components/atoms/Card/Card';
-
+import CallToAction from './components/organisms/CallToAction';
 
 const App = () => {
   return (
@@ -19,9 +17,7 @@ const App = () => {
           ))}
         </Routes>
       </Suspense>
-      {SERVICES.map((service) => (
-        <Card key={service.id} service={service} />
-      ))}
+      <CallToAction />
       <Footer />
     </BrowserRouter>
   );
