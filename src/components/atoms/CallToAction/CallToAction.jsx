54611b525cd4ce708dc38d10966177a4c750312b
Rokas <rokasgiedraitis@gmail.com>
2022-08-10 19:09:36 +0300

    Created CallToAction component and styled component

---
+++ src/components/atoms/CallToAction/CallToAction.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleCallToAction } from './CallToAction.styled';
+
+const CallToAction = () => {
+  return <StyleCallToAction>CallToAction</StyleCallToAction>;
+};
+
+export default CallToAction;
