4d0a8096ee3c7d445deb4d6d44f8fcf71779c486
Skaiste <smacaityte@gmail.com>
2022-08-10 21:12:18 +0300

    Modified App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ROUTES from './shared/constants/routes';
-import './App.css';
-import Header from './components/organisms/Header/Header';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ROUTES from "./shared/constants/routes";
+import "./App.css";
+import Header from "./components/organisms/Header/Header";
+import Footer from "./components/organisms/Footer/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           ))}
         </Routes>
       </Suspense>
+      <Footer />
     </BrowserRouter>
   );
 };
