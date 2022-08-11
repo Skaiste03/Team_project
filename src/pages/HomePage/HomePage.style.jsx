c74ddaf61d7be7a2c33ab752456a99bf264e4742
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 11:31:47 +0300

    Updated file name

--- src/pages/HomePage/HomePage.style.jsx
+++ src/pages/HomePage/HomePage.style.jsx
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledHomePage = styled.div``;
+
+export const StyledHeroContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 15px;
+`;
