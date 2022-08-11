c650412910888c42c71bfbe44c14e99fb937c8d9
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 17:07:09 +0300

    Created reusable ServiceCard component

---
+++ src/components/atoms/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledServiceCard } from './ServiceCard.style';
+
+const ServiceCard = () => {
+  return <StyledServiceCard>ServiceCard</StyledServiceCard>;
+};
+
+export default ServiceCard;
---
+++ src/components/atoms/ServiceCard/ServiceCard.style.jsx
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const StyledServiceCard = styled.div``;
