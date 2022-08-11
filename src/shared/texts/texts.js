51a00d75fe8250466c264c9c8846ce2d290491a7
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 11:32:08 +0300

    Added more texts into texts.js

--- src/shared/texts/texts.js
+++ src/shared/texts/texts.js
@@ -1,25 +1,28 @@
 const TEXTS = {
   navigation: {
-    home: "Home",
-    aboutus: "About us",
-    services: "Services",
-    blog: "Blog",
-    contact: "Contact",
+    home: 'Home',
+    aboutus: 'About us',
+    services: 'Services',
+    blog: 'Blog',
+    contact: 'Contact',
   },
+
   header: {
-    service: "Road Assistance",
-    phone: "1800 265 24 52",
+    service: 'Road Assistance',
+    phone: '1800 265 24 52',
   },
+
   footer: {
-    heading: "Get in touch with us for your service",
+    heading: 'Get in touch with us for your service',
     titles: {
-      help: "Help line Number",
-      address: "Address",
-      open: "We are open",
+      help: 'Help line Number',
+      address: 'Address',
+      open: 'We are open',
     },
-    address: "NH 234 Public Square San Francisco 65368",
-    openhours: "Monday to Friday 9:00 AM to 10:00 AM",
+    address: 'NH 234 Public Square San Francisco 65368',
+    openhours: 'Monday to Friday 9:00 AM to 10:00 AM',
   },
+
   services: {
     diag: 'Diagnostics',
     engine: 'Engine Repair',
@@ -31,6 +34,15 @@ const TEXTS = {
     custom: 'Custom Service',
     wash: 'Car wash',
     ac: 'AC Repair',
+  },
+
+  homePage: {
+    hero: {
+      heading: 'Get your vehicle service done online at one place',
+      button: 'Book a service',
+      openTitle: 'Monday to Friday 9:00 AM to 10:00  AM',
+      openSubTitle: 'We are open',
+    },
   },
 };
 
