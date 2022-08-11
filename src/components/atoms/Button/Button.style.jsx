78c44cb539515a7b9c5121391c7cda786b738904
Skaiste <smacaityte@gmail.com>
2022-08-11 18:17:27 +0300

    Updated button style

--- src/components/atoms/Button/Button.style.jsx
+++ src/components/atoms/Button/Button.style.jsx
@@ -1,15 +1,32 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-  width: ${(props) => (props.small ? '177' : '203')}px;
+  width: ${(props) =>
+    props.size === 'sm' ? '177' : props.size === 'lg' ? '208' : props.size}px;
+  height: 64px;
 
   font-size: 18px;
+  font-weight: 600;
+  color: #fff;
 
   border: none;
   border-radius: 12px;
 
   padding: 16px 40px;
 
-  color: #fff;
-  background: ${(props) => (props.color == 'primary' ? '#FF6433' : '#1E1B1B')};
+  cursor: pointer;
+
+  background: ${(props) =>
+    props.bg === 'primary'
+      ? '#FF6433'
+      : props.bg === 'black'
+      ? '#1E1B1B'
+      : props.bg};
+
+  transition: 0.3s ease-in;
+
+  &:hover {
+    opacity: 0.9;
+    transform: scale(0.98);
+  }
 `;
