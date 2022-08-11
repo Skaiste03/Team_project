6f98cf9db9b5011524395d01c99a2408b1159c89
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 22:45:49 +0300

    Created Process section starting dir

---
+++ src/pages/HomePage/components/organisms/Process/Process.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledProcess } from './Process.style';
+
+const Process = () => {
+  return <StyledProcess>Process</StyledProcess>;
+};
+
+export default Process;
---
+++ src/pages/HomePage/components/organisms/Process/Process.style.jsx
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const StyledProcess = styled.div``;
