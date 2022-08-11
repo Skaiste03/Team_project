9ede0886d922da2b329108b6be06b63ca5b62ac4
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 17:59:13 +0300

    Deleted component ServiceList

--- src/components/organisms/ServiceList/ServiceList.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledServiceList } from './ServiceList.style';
-
-const ServiceList = () => {
-  return <StyledServiceList>ServiceList</StyledServiceList>;
-};
-
-export default ServiceList;
