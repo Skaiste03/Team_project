3b9ba3dfa66e4568fa6881a8ffaf96afc7ed7e4b
Skaiste <smacaityte@gmail.com>
2022-08-11 14:23:00 +0300

    Added some text

--- src/shared/texts/texts.js
+++ src/shared/texts/texts.js
@@ -1,24 +1,25 @@
 const TEXTS = {
   navigation: {
-    home: "Home",
-    aboutus: "About us",
-    services: "Services",
-    blog: "Blog",
-    contact: "Contact",
+    home: 'Home',
+    aboutus: 'About us',
+    services: 'Services',
+    blog: 'Blog',
+    contact: 'Contact',
   },
   header: {
-    service: "Road Assistance",
-    phone: "1800 265 24 52",
+    service: 'Road Assistance',
+    phone: '1800 265 24 52',
   },
   footer: {
-    heading: "Get in touch with us for your service",
+    heading: 'Get in touch with us for your service',
     titles: {
-      help: "Help line Number",
-      address: "Address",
-      open: "We are open",
+      help: 'Help line Number',
+      address: 'Address',
+      open: 'We are open',
     },
-    address: "NH 234 Public Square San Francisco 65368",
-    openhours: "Monday to Friday 9:00 AM to 10:00 AM",
+    address: 'NH 234 Public Square San Francisco 65368',
+    openhours: 'Monday to Friday 9:00 AM to 10:00 AM',
+    cop: 'Copyright Finsweet',
   },
   services: {
     diag: 'Diagnostics',
