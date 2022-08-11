a9a56deaa9e12161947adce9eff47d1fb1814089
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 15:20:42 +0300

    Add rule for padding in mobile version

--- src/pages/HomePage/HomePage.style.jsx
+++ src/pages/HomePage/HomePage.style.jsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
+import BREAKPOINTS from '../../shared/constants/breakpoints';
 
 export const StyledHomePage = styled.div``;
 
 export const StyledHeroContainer = styled.div`
   max-width: 1280px;
   margin: 0 auto;
-  padding: 0 15px;
+  padding: 0 30px;
+
+  @media screen and (min-width: ${BREAKPOINTS.md}) {
+    padding: 0 15px;
+  }
 `;
