fc8fddca02b3c28d5c7218198ee1ea863db875f1
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 22:22:47 +0300

    Updated reusable form with dynamic inputs

--- src/components/organisms/Form/Form.jsx
+++ src/components/organisms/Form/Form.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { StyledForm } from './Form.style';
+import { StyledForm, StyledInput, StyledInputsWrapper } from './Form.style';
 
-const Form = () => {
-  return <StyledForm>Form</StyledForm>;
+const Form = ({ inputs, buttonText }) => {
+  const handleSubmit = (e) => e.preventDefault();
+
+  return (
+    <StyledForm onClick={handleSubmit}>
+      <StyledInputsWrapper>
+        {inputs.map((input) => (
+          <StyledInput
+            key={input.id}
+            placeholder={input.placeholder}
+            type={input.type}
+          ></StyledInput>
+        ))}
+      </StyledInputsWrapper>
+      <button>{buttonText}</button>
+    </StyledForm>
+  );
 };
 
 export default Form;
