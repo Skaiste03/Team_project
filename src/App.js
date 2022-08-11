3411c6561443d32f4e80dee50e7bd142408b345a
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 16:25:30 +0300

    Removed old Footer component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ROUTES from './shared/constants/routes';
 import './App.css';
 import Header from './components/organisms/Header/Header';
-import Footer from './components/organisms/Footer/Footer';
 
 const App = () => {
   return (
@@ -16,7 +15,6 @@ const App = () => {
           ))}
         </Routes>
       </Suspense>
-      <Footer />
     </BrowserRouter>
   );
 };
