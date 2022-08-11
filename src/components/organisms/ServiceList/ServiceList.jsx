d966405bde7e3e8a1ebe7bb306e6fa674fb78cd5
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 17:07:55 +0300

    Created ServiceList reusable component

---
+++ src/components/organisms/ServiceList/ServiceList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledServiceList } from './ServiceList.style';
+
+const ServiceList = () => {
+  return <StyledServiceList>ServiceList</StyledServiceList>;
+};
+
+export default ServiceList;
---
+++ src/components/organisms/ServiceList/ServiceList.style.jsx
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const StyledServiceList = styled.div``;
