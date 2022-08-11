5ff005ddf8e99f93aa51311c043ad7d88df381dc
Rokas <rokasgiedraitis@gmail.com>
2022-08-11 18:01:34 +0300

    Created Services component for Home page

---
+++ src/pages/HomePage/components/organisms/Services/Services.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledServices } from './Services.style';
+
+const Services = () => {
+  return <StyledServices>Services</StyledServices>;
+};
+
+export default Services;
