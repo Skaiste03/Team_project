3c03ba1af1562fd203cb15123f5fad185bd460fe
Skaiste <smacaityte@gmail.com>
2022-08-12 19:31:01 +0300

    Updated Feature Section added title, ServiceCard

--- src/pages/AboutPage/components/organisms/Features/Features.jsx
+++ src/pages/AboutPage/components/organisms/Features/Features.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-// import EXPERIENCES from '../../../../../shared/constants/experiences';
+import EXPERIENCES from '../../../../../shared/constants/experiences';
 import TEXTS from '../../../../../shared/texts/texts';
-import { StyledHeading, StyledSection } from './Features.style';
+import ServiceCard from '../../atoms/ServiceCard/ServiceCard';
+import {
+  StyledHeading,
+  StyledSection,
+  StyledServiceCardDiv,
+} from './Features.style';
 
 const Features = () => {
   return (
     <StyledSection>
       <div>
         <StyledHeading>{TEXTS.aboutPage.features.title}</StyledHeading>
-        <div></div>
+        <StyledServiceCardDiv>
+          {EXPERIENCES.map((experience) => (
+            <ServiceCard
+              key={experience.id}
+              icon={experience.icon}
+              title={experience.title}
+              description={experience.aboutDesc}
+            />
+          ))}
+        </StyledServiceCardDiv>
         <div></div>
       </div>
     </StyledSection>
