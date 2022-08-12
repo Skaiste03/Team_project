6f280f0e9d9dd6b4bb76815aaaed433eb092bbeb
Rokas <rokasgiedraitis@gmail.com>
2022-08-12 14:55:32 +0300

    Created reusable ExperienceCard button

---
+++ src/components/atoms/ExperienceCard/ExperienceCard.style.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import BREAKPOINTS from '../../../shared/constants/breakpoints';
+
+export const StyledExperienceCard = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 2px;
+`;
+
+export const StyledExperienceCardTitle = styled.h2`
+  color: #661ce7;
+  font-weight: 800;
+  font-size: 38px;
+  line-height: 56px;
+
+  letter-spacing: -0.03em;
+
+  @media screen and (min-width: ${BREAKPOINTS.lg}) {
+    font-size: 58px;
+    line-height: 76px;
+  }
+`;
+
+export const StyledExperienceCardDesc = styled.p`
+  color: #1e1b1b;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+
+  opacity: 0.7;
+
+  @media screen and (min-width: ${BREAKPOINTS.lg}) {
+    font-size: 24px;
+    line-height: 36px;
+  }
+`;
