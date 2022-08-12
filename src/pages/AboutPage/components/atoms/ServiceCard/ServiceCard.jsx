08d032044da948da6fd546722ba4c2fd120ee690
Skaiste <smacaityte@gmail.com>
2022-08-12 19:28:14 +0300

    Added reusable image title description into ServiceCard

--- src/pages/AboutPage/components/atoms/ServiceCard/ServiceCard.jsx
+++ src/pages/AboutPage/components/atoms/ServiceCard/ServiceCard.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { StyledServiceCard } from './ServiceCard.style';
 
-const ServiceCard = () => {
-  return <div>ServiceCard</div>;
+const ServiceCard = (props) => {
+  return (
+    <StyledServiceCard>
+      <img src={props.icon} alt='icon' />
+      <h4>{props.title}</h4>
+      <p>{props.description}</p>
+    </StyledServiceCard>
+  );
 };
 
 export default ServiceCard;
