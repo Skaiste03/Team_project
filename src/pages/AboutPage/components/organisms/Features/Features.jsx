39222cf477cebe5e540e66409cc402725209b3b8
Skaiste <smacaityte@gmail.com>
2022-08-12 21:08:23 +0300

    Added Experience component to Features Section

--- src/pages/AboutPage/components/organisms/Features/Features.jsx
+++ src/pages/AboutPage/components/organisms/Features/Features.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import EXPERIENCES from '../../../../../shared/constants/experiences';
+import HOME_PAGE_STATS from '../../../../../shared/constants/stats';
 import TEXTS from '../../../../../shared/texts/texts';
 import ServiceCard from '../../atoms/ServiceCard/ServiceCard';
+import ExperienceCard from '../../../../../components/atoms/ExperienceCard';
+
 import {
   StyledHeading,
   StyledSection,
   StyledServiceCardDiv,
+  StyledNumbersDiv,
 } from './Features.style';
 
 const Features = () => {
   return (
     <StyledSection>
-      <div>
-        <StyledHeading>{TEXTS.aboutPage.features.title}</StyledHeading>
-        <StyledServiceCardDiv>
-          {EXPERIENCES.map((experience) => (
-            <ServiceCard
-              key={experience.id}
-              icon={experience.icon}
-              title={experience.title}
-              description={experience.aboutDesc}
-            />
-          ))}
-        </StyledServiceCardDiv>
-        <div></div>
-      </div>
+      <StyledHeading>{TEXTS.aboutPage.features.title}</StyledHeading>
+      <StyledServiceCardDiv>
+        {EXPERIENCES.map((experience) => (
+          <ServiceCard
+            key={experience.id}
+            icon={experience.icon}
+            title={experience.title}
+            description={experience.aboutDesc}
+          />
+        ))}
+      </StyledServiceCardDiv>
+      <StyledNumbersDiv>
+        {HOME_PAGE_STATS.map((stat) => (
+          <ExperienceCard key={stat.id} title={stat.title} desc={stat.desc} />
+        ))}
+      </StyledNumbersDiv>
     </StyledSection>
   );
 };
