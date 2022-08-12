e92dfc5eb6563fc60755b69bfd0aecaffe318a87
Skaiste <smacaityte@gmail.com>
2022-08-12 23:59:50 +0300

    Added AboutPage desc

--- src/shared/constants/quotes.js
+++ src/shared/constants/quotes.js
@@ -6,21 +6,18 @@ export const HOME_PAGE_QUOTES = [
     id: uid(),
     title: `${TEXTS.homePage.process.firstProcess.title}`,
     desc: `${TEXTS.homePage.process.firstProcess.desc}`,
+    aboutDesc: `${TEXTS.aboutPage.steps.quotes.firstDesc}`,
   },
   {
     id: uid(),
     title: `${TEXTS.homePage.process.secondProcess.title}`,
     desc: `${TEXTS.homePage.process.secondProcess.desc}`,
+    aboutDesc: `${TEXTS.aboutPage.steps.quotes.firstDesc}`,
   },
   {
     id: uid(),
     title: `${TEXTS.homePage.process.thirdProcess.title}`,
     desc: `${TEXTS.homePage.process.thirdProcess.desc}`,
+    aboutDesc: `${TEXTS.aboutPage.steps.quotes.firstDesc}`,
   },
-];
-
-export const ABOUTUS_PAGE_QUOTES = [
-  { id: uid(), title: ``, desc: `` },
-  { id: uid(), title: ``, desc: `` },
-  { id: uid(), title: ``, desc: `` },
 ];
