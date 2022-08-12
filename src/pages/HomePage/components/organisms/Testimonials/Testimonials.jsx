b5f3bb263193bf4851896925a8d8dfbd0c301818
Rokas <rokasgiedraitis@gmail.com>
2022-08-12 19:56:06 +0300

    Created Testimonials section starting folder

---
+++ src/pages/HomePage/components/organisms/Testimonials/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledTestimonials } from './Testimonials.style';
+import StyledContainer from '../../../../../assets/styles/StyledContainer';
+
+const Testimonials = () => {
+  return (
+    <StyledContainer>
+      <StyledTestimonials>Testimonials</StyledTestimonials>
+    </StyledContainer>
+  );
+};
+
+export default Testimonials;
---
+++ src/pages/HomePage/components/organisms/Testimonials/Testimonials.style.jsx
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+// import BREAKPOINTS from '../../../../../shared/constants/breakpoints';
+
+export const StyledTestimonials = styled.div``;
