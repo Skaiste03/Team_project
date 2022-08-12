8b2a3ed8117deaa4e8b7d58cd7fe18724ffe5e3a
Rokas <rokasgiedraitis@gmail.com>
2022-08-13 02:37:00 +0300

    Updated Testimonials component. Finished slider logic

--- src/pages/HomePage/components/organisms/Testimonials/Testimonials.jsx
+++ src/pages/HomePage/components/organisms/Testimonials/Testimonials.jsx
@@ -1,11 +1,145 @@
-import React from 'react';
-import { StyledTestimonials } from './Testimonials.style';
+import { useState, useEffect, useCallback } from 'react';
+import {
+  StyledTestimonialBottom,
+  StyledTestimonialButtonsWrapper,
+  StyledTestimonialPointer,
+  StyledTestimonialButton,
+  StyledTestimonials,
+  StyledTestimonialsHeading,
+  StyledTestimonialWrapper,
+} from './Testimonials.style';
 import StyledContainer from '../../../../../assets/styles/StyledContainer';
+import TEXTS from '../../../../../shared/texts/texts';
+import SliderCard from '../../atoms/SliderCard';
+import SLIDER_DATA from '../../../../../data/slider';
+import useViewport from '../../../../../hooks/useViewport';
+import usePagination from '../../../../../hooks/usePagination';
+import BREAKPOINTS from '../../../../../shared/constants/breakpoints';
+import { SmallArrowIcon } from '../../../../../assets/icons';
+import { motion } from 'framer-motion';
+
+const TestimonialsMotion = motion(StyledTestimonialWrapper);
 
 const Testimonials = () => {
+  // -- States
+  const [page, setPage] = useState(0);
+  const [touchStart, setTouchStart] = useState(null);
+  const [isNext, setIsNext] = useState(true);
+
+  const { isSmall } = useViewport(BREAKPOINTS.lg);
+  const data = usePagination(SLIDER_DATA, isSmall ? 1 : 2);
+
+  // -- Functions
+  const handleNextChange = (page, data) => {
+    setIsNext(true);
+
+    if (page + 1 < data.length) {
+      return setPage(page + 1);
+    }
+
+    return setPage(0);
+  };
+
+  const handlePrevChange = () => {
+    setIsNext(false);
+
+    if (page <= 0) {
+      return setPage(data.length - 1);
+    }
+
+    return setPage(page - 1);
+  };
+
+  // -- onTouchStart
+  const handleTouchStart = (e) => {
+    setTouchStart(e.touches[0].clientX);
+  };
+
+  // -- onTouchEnd
+  const handleTouchEnd = (e) => {
+    if (touchStart === null) {
+      return;
+    }
+
+    const touchLength = touchStart - e.changedTouches[0].clientX;
+
+    if (touchLength > 5) {
+      return handleNextChange(page, data);
+    }
+
+    if (touchLength < 5) {
+      return handlePrevChange();
+    }
+
+    setTouchStart(null);
+  };
+
+  // -- Callback
+  const memoizedCallback = useCallback(() => {
+    handleNextChange(page, data);
+  }, [page, data]);
+
+  // -- Side effects
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      memoizedCallback();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [memoizedCallback]);
+
+  if (data.length - 1 < page) {
+    return setPage(0);
+  }
+
+  // -- Motion variants
+  const slider = {
+    next: { x: `${isSmall ? '20px' : '50'}`, opacity: 0 },
+    prev: { x: `${isSmall ? '-20px' : '-50'}`, opacity: 0 },
+  };
+
   return (
     <StyledContainer>
-      <StyledTestimonials>Testimonials</StyledTestimonials>
+      <StyledTestimonials>
+        <StyledTestimonialsHeading>
+          {TEXTS.homePage.testimonials.heading}
+        </StyledTestimonialsHeading>
+        <TestimonialsMotion
+          key={page}
+          initial={isNext ? 'next' : 'prev'}
+          animate={{ x: 0, opacity: 1 }}
+          exit={isNext ? 'next' : 'prev'}
+          variants={slider}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
+        >
+          <SliderCard data={data[page][0]} />
+          {!isSmall && <SliderCard data={data[page][1]} />}
+        </TestimonialsMotion>
+        <StyledTestimonialBottom>
+          {data.map((_, index) => (
+            <StyledTestimonialPointer
+              key={index}
+              isActive={index === page}
+            ></StyledTestimonialPointer>
+          ))}
+
+          <StyledTestimonialButtonsWrapper>
+            <StyledTestimonialButton
+              onClick={handlePrevChange}
+              clickType={'prev'}
+            >
+              <SmallArrowIcon />
+            </StyledTestimonialButton>
+            <StyledTestimonialButton
+              onClick={() => handleNextChange(page, data)}
+              clickType={'next'}
+            >
+              <SmallArrowIcon />
+            </StyledTestimonialButton>
+          </StyledTestimonialButtonsWrapper>
+        </StyledTestimonialBottom>
+      </StyledTestimonials>
     </StyledContainer>
   );
 };
