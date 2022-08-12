dd99331403f6aaad7b2535303f10067e7078fca3
Skaiste <smacaityte@gmail.com>
2022-08-12 19:32:10 +0300

    Updated style for Feature Section

--- src/pages/AboutPage/components/organisms/Features/Features.style.jsx
+++ src/pages/AboutPage/components/organisms/Features/Features.style.jsx
@@ -7,9 +7,30 @@ export const StyledSection = styled.section`
 
 export const StyledHeading = styled.h2`
   color: #1e1b1b;
-  font-size: 58px;
+
+  font-size: 35px;
   font-weight: 800;
+  line-height: 50px;
+
+  text-align: center;
+  letter-spacing: -0.03em;
+
+  margin-bottom: 64px;
 
   @media screen and (min-width: ${BRAKEPOINTS.md}) {
+    font-size: 58px;
+    line-height: 76px;
+
+    padding: 0 15% 0 15%;
+  }
+`;
+
+export const StyledServiceCardDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (min-width: ${BRAKEPOINTS.lg}) {
+    flex-direction: row;
   }
 `;
