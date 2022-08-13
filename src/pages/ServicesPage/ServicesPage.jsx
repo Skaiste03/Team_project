61bf11de90564bf0f3b55b8284daaeee9cc8a0a8
Rokas <rokasgiedraitis@gmail.com>
2022-08-13 20:22:52 +0300

    Created starting ServicePages directory

---
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SetyledServicesPage } from './ServicesPage.style';
+import StyledWrapper from '../../assets/styles/StyledWrapper';
+
+const ServicesPage = () => {
+  return (
+    <SetyledServicesPage>
+      <StyledWrapper></StyledWrapper>
+    </SetyledServicesPage>
+  );
+};
+
+export default ServicesPage;
