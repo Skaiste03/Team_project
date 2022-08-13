ef50f4dde93427c8f5bc09c20b458f935d92c012
Skaiste <smacaityte@gmail.com>
2022-08-13 20:46:30 +0300

    Added About Page Quotes

--- src/shared/constants/quotes.js
+++ src/shared/constants/quotes.js
@@ -6,18 +6,33 @@ export const HOME_PAGE_QUOTES = [
     id: uid(),
     title: `${TEXTS.homePage.process.firstProcess.title}`,
     desc: `${TEXTS.homePage.process.firstProcess.desc}`,
-    aboutDesc: `${TEXTS.aboutPage.steps.quotes.firstDesc}`,
   },
   {
     id: uid(),
     title: `${TEXTS.homePage.process.secondProcess.title}`,
     desc: `${TEXTS.homePage.process.secondProcess.desc}`,
-    aboutDesc: `${TEXTS.aboutPage.steps.quotes.firstDesc}`,
   },
   {
     id: uid(),
     title: `${TEXTS.homePage.process.thirdProcess.title}`,
     desc: `${TEXTS.homePage.process.thirdProcess.desc}`,
-    aboutDesc: `${TEXTS.aboutPage.steps.quotes.firstDesc}`,
+  },
+];
+
+export const ABOUT_PAGE_QUOTES = [
+  {
+    id: uid(),
+    title: `${TEXTS.homePage.process.firstProcess.title}`,
+    desc: `${TEXTS.aboutPage.steps.quotes.firstDesc}`,
+  },
+  {
+    id: uid(),
+    title: `${TEXTS.homePage.process.secondProcess.title}`,
+    desc: `${TEXTS.aboutPage.steps.quotes.secondDesc}`,
+  },
+  {
+    id: uid(),
+    title: `${TEXTS.homePage.process.thirdProcess.title}`,
+    desc: `${TEXTS.aboutPage.steps.quotes.thirdDesc}`,
   },
 ];
