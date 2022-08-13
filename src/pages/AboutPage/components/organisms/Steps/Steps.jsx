c5d88d507642ac803f4fed6c6c42758efbc7684f
Skaiste <smacaityte@gmail.com>
2022-08-13 20:45:05 +0300

    Added StyledQuoteDiv for QuoteCard

--- src/pages/AboutPage/components/organisms/Steps/Steps.jsx
+++ src/pages/AboutPage/components/organisms/Steps/Steps.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import QuoteCard from '../../../../../components/atoms/QuoteCard';
 import TEXTS from '../../../../../shared/texts/texts';
-import image from '../../../../../assets/images/cta-bg-1.jpg';
+import { ABOUT_PAGE_QUOTES } from '../../../../../shared/constants/quotes';
+
+import {
+  StyledSection,
+  StyledImageDiv,
+  StyledWrapper,
+  StyledTexts,
+  StyledQuotesDiv,
+} from './Steps.style';
 
 const Steps = () => {
   return (
-    <section>
-      <div>
-        <div>
+    <StyledSection>
+      <StyledWrapper>
+        <StyledTexts>
           <h3>{TEXTS.aboutPage.steps.title}</h3>
           <p>{TEXTS.aboutPage.steps.desc}</p>
-        </div>
-        <QuoteCard />
-      </div>
-      <div>
-        <img src='' alt='' />
-      </div>
-    </section>
+        </StyledTexts>
+        <StyledQuotesDiv>
+          {ABOUT_PAGE_QUOTES.map((quote, index) => (
+            <QuoteCard
+              key={quote.id}
+              quote={quote}
+              number={index + 1}
+              line={index !== ABOUT_PAGE_QUOTES.length - 1}
+            />
+          ))}
+        </StyledQuotesDiv>
+      </StyledWrapper>
+      <StyledImageDiv></StyledImageDiv>
+    </StyledSection>
   );
 };
 
