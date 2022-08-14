26a9ad84d4ddd45c7d33333a3164af6f2b105f9c
Rokas <rokasgiedraitis@gmail.com>
2022-08-14 14:53:02 +0300

    Created HowWorksCard reusable component

---
+++ src/pages/ServicesPage/components/HowWorksCard/HowWorksCard.style.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const StyledHowWorksCard = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 24px;
+
+  margin-bottom: 32px;
+`;
+
+export const StyledHowWorksCardCricle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.color.green};
+
+  width: 48px;
+  height: 48px;
+
+  border-radius: 50%;
+
+  & svg {
+    color: ${({ theme }) => theme.color.white};
+  }
+`;
+
+export const StyledHowWorksCardTitle = styled.div`
+  ${({ theme }) => theme.title2}
+  margin-top: 5px;
+`;
