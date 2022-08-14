2842149d25c86486187491ab9db507532f150081
Rokas <rokasgiedraitis@gmail.com>
2022-08-14 10:28:16 +0300

    Added HowWorks section to the ServicesPage

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { SetyledServicesPage } from './ServicesPage.style';
 import StyledWrapper from '../../assets/styles/StyledWrapper';
 import Services from './components/Services';
+import HowWorks from './components/HowWorks';
 
 const ServicesPage = () => {
   return (
     <SetyledServicesPage>
       <StyledWrapper></StyledWrapper>
 
-      {/* <StyledWrapper theme={'grey'}>
+      <StyledWrapper theme={'grey'}>
         <Services />
-      </StyledWrapper> */}
+      </StyledWrapper>
+
+      <StyledWrapper>
+        <HowWorks />
+      </StyledWrapper>
     </SetyledServicesPage>
   );
 };
