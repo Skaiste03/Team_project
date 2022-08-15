013945c3afa2c89293cd3b75b5100353b0084cf5
Rokas <rokasgiedraitis@gmail.com>
2022-08-15 16:00:01 +0300

    Updated Layout for responsivnes

--- src/pages/ServicePage/components/Article/Article.jsx
+++ src/pages/ServicePage/components/Article/Article.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import { StyledArticle } from './Article.style';
+import {
+  StyledArticle,
+  StyledArticleHeading,
+  StyledArticleParagraph,
+  StyledArticleQuote,
+  StyledArticleSubHeading,
+  StyledArticleSubParagraph,
+} from './Article.style';
 
-const Article = () => {
-  return <StyledArticle>Article</StyledArticle>;
+const Article = ({ service }) => {
+  return (
+    <StyledArticle>
+      <img src={service.img.image} alt={service.img.alt} />
+      <StyledArticleHeading>{service.title1}</StyledArticleHeading>
+      <StyledArticleParagraph>{service.paragraph1}</StyledArticleParagraph>
+      <StyledArticleSubHeading>{service.title2}</StyledArticleSubHeading>
+      <StyledArticleSubParagraph>
+        {service.paragraph2}
+      </StyledArticleSubParagraph>
+      <StyledArticleQuote>{service.quote}</StyledArticleQuote>
+    </StyledArticle>
+  );
 };
 
 export default Article;
