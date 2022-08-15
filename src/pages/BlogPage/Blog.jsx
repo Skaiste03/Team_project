688b5a48a7991995463e3b05f5c5ca754af6c407
Skaiste <smacaityte@gmail.com>
2022-08-15 21:21:39 +0300

    Added PostsRecaps Section to BlogPage

--- src/pages/BlogPage/Blog.jsx
+++ src/pages/BlogPage/Blog.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import StyledWrapper from '../../assets/styles/StyledWrapper';
+import StyledContainer from '../../assets/styles/StyledContainer';
 import OurBlog from './components/OurBlog';
+import PostsRecaps from './components/PostsRecaps/PostsRecaps';
+
 const Blog = () => {
   return (
     <main>
       <StyledWrapper>
         <OurBlog />
+      </StyledWrapper>
+
+      <StyledWrapper theme='grey'>
+        <StyledContainer>
+          <PostsRecaps />
+        </StyledContainer>
       </StyledWrapper>
     </main>
   );
