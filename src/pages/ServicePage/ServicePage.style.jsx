63eafd3b2b7aa80e33c24777d3651b8a18f346a9
Rokas <rokasgiedraitis@gmail.com>
2022-08-15 22:05:25 +0300

    CallToAction component moved into main tag

--- src/pages/ServicePage/ServicePage.style.jsx
+++ src/pages/ServicePage/ServicePage.style.jsx
@@ -3,15 +3,6 @@ import BREAKPOINTS from '../../shared/constants/breakpoints';
 
 export const StyledServicePage = styled.main`
   padding-top: 50px;
-  padding-bottom: 50px;
-
-  @media screen and (min-width: ${BREAKPOINTS.md}) {
-    padding-bottom: 100px;
-  }
-
-  @media screen and (min-width: ${BREAKPOINTS.lg}) {
-    padding-bottom: 128px;
-  }
 `;
 
 export const StyledServicePageHeading = styled.h1`
