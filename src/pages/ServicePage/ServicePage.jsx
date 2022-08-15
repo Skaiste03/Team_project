23a99445490b5b41b94cbd43b1f1f58058774627
Rokas <rokasgiedraitis@gmail.com>
2022-08-15 16:00:54 +0300

    Updated Layout for responsivnes

--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,28 +1,28 @@
 import {
   StyledServicePage,
   StyledServicePageHeading,
-  StyledServicePageParagraph1,
 } from './ServicePage.style';
 import StyledContainer from '../../assets/styles/StyledContainer';
 import { useParams } from 'react-router-dom';
 import SERVICES from '../../shared/constants/services';
+import Article from './components/Article/Article';
+import Layout from './components/Layout';
+import Sidebar from './components/Sidebar';
 
 const ServicePage = () => {
   const { id } = useParams();
 
   const service = SERVICES.find((service) => service.id === +id);
+  const otherServices = SERVICES.filter((service) => service.id !== +id);
 
   return (
     <StyledServicePage>
       <StyledContainer>
-        <aside></aside>
-        <article>
-          <img src={service.img.image} alt={service.img.alt} />
-          <StyledServicePageHeading>{service.text}</StyledServicePageHeading>
-          <StyledServicePageParagraph1>
-            {service.paragraph1}
-          </StyledServicePageParagraph1>
-        </article>
+        <StyledServicePageHeading>{service.text}</StyledServicePageHeading>
+        <Layout>
+          <Sidebar otherServices={otherServices} />
+          <Article service={service} />
+        </Layout>
       </StyledContainer>
     </StyledServicePage>
   );
