55e86f9132a0521579a77d9fdc44a9222b1f91aa
Rokas <rokasgiedraitis@gmail.com>
2022-08-15 14:00:10 +0300

    Created Article, Sidebar and Layout starting dirs

---
+++ src/pages/ServicePage/components/Article/Article.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledArticle } from './Article.style';
+
+const Article = () => {
+  return <StyledArticle>Article</StyledArticle>;
+};
+
+export default Article;
