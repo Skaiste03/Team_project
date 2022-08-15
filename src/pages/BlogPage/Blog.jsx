7536e0201c8a92d76a002db82a043024edd5120b
Skaiste <smacaityte@gmail.com>
2022-08-15 15:44:37 +0300

    Created BlogPage dir, BlogPage component, OurBlog Section, Components styles and index.js

---
+++ src/pages/BlogPage/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StyledWrapper from '../../assets/styles/StyledWrapper';
+import OurBlog from './components/OurBlog';
+const Blog = () => {
+  return (
+    <main>
+      <StyledWrapper>
+        <OurBlog />
+      </StyledWrapper>
+    </main>
+  );
+};
+
+export default Blog;
