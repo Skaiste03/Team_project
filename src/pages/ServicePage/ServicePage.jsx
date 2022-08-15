a3d5cdc03dd1639d7233f704971a4e0cbdeff16b
Rokas <rokasgiedraitis@gmail.com>
2022-08-15 09:58:07 +0300

    Created starting single service page dir

---
+++ src/pages/ServicePage/ServicePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyledServicePage } from './ServicePage.style';
+import StyledContainer from '../../assets/styles/StyledContainer';
+import StyledWrapper from '../../assets/styles/StyledWrapper';
+
+const ServicePage = () => {
+  return (
+    <StyledServicePage>
+      <StyledWrapper>
+        <StyledContainer></StyledContainer>
+      </StyledWrapper>
+    </StyledServicePage>
+  );
+};
+
+export default ServicePage;
