b00ccefc3bb33e6afdca532b3b54219fd739e8cf
Rokas <rokasgiedraitis@gmail.com>
2022-08-16 22:57:21 +0300

    Updated ContactPage with Contacts component

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { StyledContactPage } from './ContactPage.style';
+import StyledContainer from '../../assets/styles/StyledContainer';
+import StyledWrapper from '../../assets/styles/StyledWrapper';
+import Contacts from './components/Contacts';
 
 const ContactPage = () => {
-  return <StyledContactPage>Contact</StyledContactPage>;
+  return (
+    <StyledContactPage>
+      <StyledWrapper>
+        <StyledContainer>
+          <Contacts />
+        </StyledContainer>
+      </StyledWrapper>
+    </StyledContactPage>
+  );
 };
 
 export default ContactPage;
