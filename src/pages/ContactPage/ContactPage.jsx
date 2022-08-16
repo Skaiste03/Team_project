e6d557773874273fba4bddc2573603226168f515
Rokas <rokasgiedraitis@gmail.com>
2022-08-16 21:00:22 +0300

    Created starting ContactPage dir

---
+++ src/pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledContactPage } from './ContactPage.style';
+
+const ContactPage = () => {
+  return <StyledContactPage>Contact</StyledContactPage>;
+};
+
+export default ContactPage;
