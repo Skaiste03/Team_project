f3d9834216dae9d527b95d4536ab1e6e70d838ed
Rokas <rokasgiedraitis@gmail.com>
2022-08-18 17:30:50 +0300

    Updated Maps component with useMemo logic

--- src/pages/ContactPage/components/Maps/Maps.jsx
+++ src/pages/ContactPage/components/Maps/Maps.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import {
   StyledMaps,
   StyledMapsComponent,
@@ -14,31 +14,40 @@ import {
 } from '@react-google-maps/api';
 import MapMarker from '../MapMarker';
 
-// GoogleMap component style
-const containerStyle = {
-  width: '100%',
-  height: '100%',
-};
-
-// GoogleMap initial position and Marker position
-const center = {
-  lat: 40.701,
-  lng: -73.995,
-};
-
-// GoogleMap options
-const options = {
-  mapId: '75339764bd754f8', // Map style id
-  disableDefaultUI: true,
-  clickableIcons: false,
-};
-
 const Maps = () => {
   const [selected, setSelected] = useState(false);
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
   });
+
+  // GoogleMap container style
+  const containerStyle = useMemo(
+    () => ({
+      width: '100%',
+      height: '100%',
+    }),
+    []
+  );
+
+  // GoogleMap initial position and Marker position
+  const center = useMemo(
+    () => ({
+      lat: 40.701,
+      lng: -73.995,
+    }),
+    []
+  );
+
+  // GoogleMap options
+  const options = useMemo(
+    () => ({
+      mapId: '75339764bd754f8', // Map style id
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
 
   return (
     <StyledMaps>
