87ff1795ea84795ce6048a5350938ac0b4ab0aa1
Rokas <rokasgiedraitis@gmail.com>
2022-08-18 16:27:17 +0300

    Created Maps section

---
+++ src/pages/ContactPage/components/Maps/Maps.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import {
+  StyledMaps,
+  StyledMapsComponent,
+  StyledMapsContainer,
+} from './Maps.style';
+import contactBg from '../../../../assets/images/contact-bg.jpg';
+
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import MapMarker from '../Marker';
+
+const containerStyle = {
+  width: '100%',
+  height: '100%',
+};
+
+const center = {
+  lat: 40.701,
+  lng: -73.995,
+};
+
+const options = {
+  mapId: '75339764bd754f8',
+  disableDefaultUI: true,
+  clickableIcons: false,
+};
+
+const Maps = () => {
+  const [selected, setSelected] = useState(false);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+  });
+
+  return (
+    <StyledMaps>
+      <StyledMapsContainer>
+        <StyledMapsComponent>
+          {isLoaded && (
+            <GoogleMap
+              zoom={13}
+              center={center}
+              options={options}
+              mapContainerStyle={containerStyle}
+            >
+              <Marker
+                position={center}
+                onClick={() => setSelected(!selected)}
+              />
+
+              {selected && (
+                <InfoWindow
+                  position={center}
+                  onCloseClick={() => setSelected(false)}
+                >
+                  <MapMarker />
+                </InfoWindow>
+              )}
+            </GoogleMap>
+          )}
+        </StyledMapsComponent>
+      </StyledMapsContainer>
+      <img src={contactBg} alt={'contactBg'} />
+    </StyledMaps>
+  );
+};
+
+export default Maps;
